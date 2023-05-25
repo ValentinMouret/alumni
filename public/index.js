ce298e1aaa021df447ba2a26eeef231a06fab9df
Valentin Mouret <valentin.mou@gmail.com>
2023-05-25 16:01:25 +0200

    Add a counter of days until deadline
    Add a counter of tickets sold. It has to be manually updated.

---
+++ public/index.js
@@ -0,0 +1,21 @@
+const currentDate = new Date()
+const deadline = new Date('2023-06-10')
+
+function dateDifferenceInDays(d1, d2) {
+     const t2 = d2.getTime()
+     const t1 = d1.getTime()
+
+    return Math.floor((t2-t1) / (24 * 3_600 * 1_000)) + 2 // current day and deadline day
+}
+
+function setTimeUntilDeadline() {
+    const node = document.getElementById("days-left")
+    const difference = dateDifferenceInDays(currentDate, deadline)
+    node.textContent = difference > 0 ? `encore ${difference} jour${difference == 1 ? '' : 's'}` : "billeterie fermée"
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeUntilDeadline()
+    // Every hour, refresh
+    setInterval(setTimeUntilDeadline, 60 * 60 * 1_000)
+})
