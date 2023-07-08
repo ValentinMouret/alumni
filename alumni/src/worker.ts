0627eacd504575dcf734ab4088e3bc6eba861c63
Valentin Mouret <valentin.mou@gmail.com>
2023-07-08 08:29:27 +0200

    Add handler for preflight requests

--- alumni/src/worker.ts
+++ alumni/src/worker.ts
@@ -9,21 +9,21 @@
  */
 
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-  //
-  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-  // MY_SERVICE: Fetcher;
-  //
-  // Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-  // MY_QUEUE: Queue;
-  DB: D1Database;
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+	//
+	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+	// MY_SERVICE: Fetcher;
+	//
+	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
+	// MY_QUEUE: Queue;
+	DB: D1Database;
 }
 
 // What do we want to do?
@@ -33,102 +33,105 @@ export interface Env {
 // - We want to check people in.
 
 interface Ticket {
-  firstName: string;
-  lastName: string;
+	firstName: string;
+	lastName: string;
 }
 
 interface CheckIn {
-  ticketId: number;
+	ticketId: number;
 }
 
 function parseTicket(params: any): Ticket | undefined {
-  if (params?.first_name && params?.last_name) {
-    return { firstName: params.first_name, lastName: params.last_name };
-  }
-  return undefined;
+	if (params?.first_name && params?.last_name) {
+		return { firstName: params.first_name, lastName: params.last_name };
+	}
+	return undefined;
 }
 
 function parseCheckIn(params: any): CheckIn | undefined {
-  if (params?.ticket_id) {
-    return { ticketId: params.ticket_id };
-  }
-  return undefined;
+	if (params?.ticket_id) {
+		return { ticketId: params.ticket_id };
+	}
+	return undefined;
 }
 
 async function insertTicket(request: Request, env: Env) {
-  const ticket = parseTicket(await request.json());
-  if (!ticket) return new Response("Invalid ticket data", { status: 422 });
+	const ticket = parseTicket(await request.json());
+	if (!ticket) return new Response('Invalid ticket data', { status: 422 });
 
-  await env.DB.prepare(
-    `insert into tickets (first_name, last_name)
-                      values (?, ?);`,
-  )
-    .bind(ticket.firstName, ticket.lastName)
-    .run();
+	await env.DB.prepare(
+		`insert into tickets (first_name, last_name)
+                      values (?, ?);`
+	)
+		.bind(ticket.firstName, ticket.lastName)
+		.run();
 }
 
 async function listTickets(request: Request, env: Env) {
-  const { results } = await env.DB.prepare(
-    `select id,
+	const { results } = await env.DB.prepare(
+		`select id,
             first_name,
             last_name,
             datetime(check_ins.created_at, 'localtime') as checked_in_at
        from tickets
             left join check_ins
-                   on tickets.id = check_ins.ticket_id;`,
-  ).all();
-  const response = Response.json(results);
-  response.headers.append("Access-Control-Allow-Origin", "*");
-  return response;
+                   on tickets.id = check_ins.ticket_id;`
+	).all();
+	const response = Response.json(results);
+	response.headers.append('Access-Control-Allow-Origin', '*');
+	return response;
 }
 
 async function checkIn(request: Request, env: Env) {
-  const checkIn = parseCheckIn(await request.json());
-  if (!checkIn) return new Response("Invalid check in data", { status: 422 });
-  try {
-    await env.DB.prepare(
-      `insert into check_ins (ticket_id, created_at)
-							values (?, datetime('now'));`,
-    )
-      .bind(checkIn.ticketId)
-      .run();
-    const response = new Response();
-    response.headers.append("Access-Control-Allow-Origin", "*");
-    return response;
-  } catch (e: any) {
-    if (e.message.includes("UNIQUE constraint failed")) {
-      return new Response("Ticket already checked in", { status: 403 });
-    }
-    throw e;
-  }
+	const checkIn = parseCheckIn(await request.json());
+	if (!checkIn) return new Response('Invalid check in data', { status: 422 });
+	try {
+		await env.DB.prepare(
+			`insert into check_ins (ticket_id, created_at)
+							values (?, datetime('now'));`
+		)
+			.bind(checkIn.ticketId)
+			.run();
+		const response = new Response();
+		response.headers.append('Access-Control-Allow-Origin', '*');
+		return response;
+	} catch (e: any) {
+		if (e.message.includes('UNIQUE constraint failed')) {
+			return new Response('Ticket already checked in', { status: 403 });
+		}
+		throw e;
+	}
 }
 
 async function deleteCheckIn(request: Request, env: Env) {
-  const checkIn = parseCheckIn(await request.json());
-  if (!checkIn) return new Response("Invalid check in data", { status: 422 });
-  await env.DB.prepare(`delete from check_ins where ticket_id = ?;`).bind(
-    checkIn.ticketId,
-  ).run();
-  const response = new Response();
-  response.headers.append("Access-Control-Allow-Origin", "*");
-  return response;
+	const checkIn = parseCheckIn(await request.json());
+	if (!checkIn) return new Response('Invalid check in data', { status: 422 });
+	await env.DB.prepare(`delete from check_ins where ticket_id = ?;`).bind(checkIn.ticketId).run();
+	const response = new Response();
+	response.headers.append('Access-Control-Allow-Origin', '*');
+	return response;
+}
+
+async function answerPreflight(request: Request, env: Env) {
+	const response = new Response();
+	response.headers.append('Access-Control-Allow-Origin', '*');
+	response.headers.append('Access-Control-Allow-Methods', 'POST, DELETE');
+	response.headers.append('Access-Control-Allow-Headers', 'Content-Type');
+	return response;
 }
 
 const routes = new Map();
-routes.set("/tickets@POST", insertTicket);
-routes.set("/tickets@GET", listTickets);
-routes.set("/check_in@POST", checkIn);
-routes.set("/check_in@DELETE", deleteCheckIn);
+routes.set('/tickets@POST', insertTicket);
+routes.set('/tickets@GET', listTickets);
+routes.set('/check_in@POST', checkIn);
+routes.set('/check_in@DELETE', deleteCheckIn);
+routes.set('/check_in@OPTIONS', answerPreflight);
 
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext,
-  ): Promise<Response> {
-    const url = new URL(request.url);
-    const handler = routes.get([url.pathname, request.method].join("@"));
-    if (!handler) return new Response("Route not found", { status: 404 });
-    return await handler(request, env, ctx);
-  },
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		const handler = routes.get([url.pathname, request.method].join('@'));
+		if (!handler) return new Response('Route not found', { status: 404 });
+		return await handler(request, env, ctx);
+	},
 };
